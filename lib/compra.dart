98d1cc43872407f7daf0e30ab94031877adc89fe
Christopher <chrissantista@gmail.com>
2018-12-08 02:54:22 -0200

    Criado compra.dart

---
+++ lib/compra.dart
@@ -0,0 +1,34 @@
+class Compra {
+  int id;
+  String descricao;
+  bool status;
+
+  Compra(this.id, this.descricao, this.status)
+      : assert(descricao != null),
+        assert(descricao != ''),
+        assert(id != null),
+        assert(id > 0),
+        assert(status != null);
+  
+  Compra.novo(this.descricao)
+      : status = false,
+        assert(descricao != null),
+        assert(descricao != '');
+  
+  Compra.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    descricao = map['descricao'];
+    status = map['status'];
+  }
+  
+  toMap() {
+    var map = <String, dynamic>{
+      'descricao': descricao,
+      'status': status,
+    };
+    if (id != null) {
+      map['id'] = id;
+    }
+    return map;
+  }
+}
