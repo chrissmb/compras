14667976abd943ea498fd9c7133ee60bcf3b2cd5
Christopher <chrissantista@gmail.com>
2018-12-08 05:15:38 -0200

    Criado metodo saveCompra em FomCompra

---
+++ lib/form_compra.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'comprasProvider.dart';
+import 'compra.dart';
+
+class FormCompra extends StatefulWidget {
+  final ComprasProvider provider;
+
+  FormCompra(this.provider) : assert(provider != null);
+
+  @override
+  FormCompraState createState() => FormCompraState();
+}
+
+class FormCompraState extends State<FormCompra> {
+  final _formKey = GlobalKey<FormState>();
+  var txtDescricaoCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digite a descrição'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: TextField(
+          controller: txtDescricaoCtrl,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: saveCompra,
+      ),
+    );
+  }
+
+  void saveCompra() {
+    widget.provider.save(
+      Compra.novo(txtDescricaoCtrl.text),
+    ).then((id) => dispose());
+  }
+}
